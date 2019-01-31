e51154d1826246701697afb7e5c44593655742e4
Eric Kouakou <erickouakou@usf.edu>
2019-01-31 18:08:35 -0500

    version 3

--- name/Program.cs
+++ name/Program.cs
@@ -6,7 +6,26 @@ namespace name
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("What is your name?!");
+            try
+            {
+
+                String name;
+                name = Console.ReadLine();
+
+
+                Console.WriteLine("Hello !" + name);
+                Console.WriteLine("press any key to continue...");
+                Console.ReadKey(true);
+
+
+
+            }
+            catch
+            {
+                Console.WriteLine("enter a name");
+                Console.ReadKey(true);
+            }
         }
     }
 }
